a9dd620217b31d97af56b0173c003c72a0ce28fb
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-08-31 02:34:24 -0400

    Add files via upload

---
+++ f.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int i,n;
+cout << "enter the number -";
+cin >> n;
+i=1;
+while (i<=n)
+{
+cout << i<< endl;
+i=i+1;
+}
+i++;
+return 0;
+}
+
+
