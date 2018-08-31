ae016cd67d3929dfb18278f719fe2a0674051d1a
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-08-31 01:26:56 -0400

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n1;
+cout << " enter any number " ;
+cin >> n1;
+if (n1%2==0)
+{
+cout <<" numbers " << n1 << " even ";
+}
+else
+{
+cout <<" numbers " << n1 << " odd ";
+}
+return 0;
+}
