e52e2129787b0d57113b8079520860076b7f49b4
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-08-31 01:25:50 -0400

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{ float n1 ,n2 ;
+cout << " enter two numbers ";
+cin >> n1 >> n2 ;
+if (n1 > n2)
+{ 
+cout << " In two numbers "<< n1 << " is the maximum number ";
+}
+else 
+{cout << " In two numbers "<< n2 <<" is the maximum number ";
+}
+return 0;
+}
