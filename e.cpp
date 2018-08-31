2643cec0d1deae0a5e244a865e86647e69afabde
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-08-31 01:27:45 -0400

    Add files via upload

---
+++ e.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float a1 , a2 , a3 ;
+cout << " angles of traingles " ;
+cin >> a1 >> a2 >> a3;
+if (a1+a2+a3 ==180)
+{
+cout << " angle of triangle " << a1 << a2 << a3 << " This is triangle " ;
+}
+else 
+{
+cout << " angle of triangle " << a1 << a2 << a3 << " This is not traingle "; 
+}
+return 0;
+}
