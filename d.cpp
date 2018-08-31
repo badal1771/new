7a317afdafe9ec66fc3cd7af2f69c451d2004e6f
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-08-31 01:27:20 -0400

    Add files via upload

---
+++ d.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+float n1;
+cout << " which Number negative ,positive or zero -" ;
+cin >> n1 ;
+if (n1>0)
+{
+cout << " The number " << n1 << " Number is positive ";
+}
+else if (n1<0)
+{
+cout << " The number " << n1 << " Number is negative ";
+}
+else 
+{
+cout << "The number " << n1 << " number is zero " ;
+}
+return 0 ;
+} 
